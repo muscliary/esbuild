muscliary/esbuild

commit 2b158364f385d37f33c1d859d1f2481eee1cbf3a

fix(parser): allow Identifier `keyof`, `readonly`, `infer` in TypeScriptObjectType (#1859)

internal/js_parser/ts_parser.go
@@ -280,7 +280,12 @@ func (p *parser) skipTypeScriptTypeWithOpts(level js_ast.L, opts skipTypeOpts) {
 
 			if kind == tsTypeIdentifierPrefix {
 				p.lexer.Next()
-				p.skipTypeScriptType(js_ast.LPrefix)
+				// {[keyof: string]: number}
+				// {[readonly: string]: number}
+				// {[infer: string]: number}
+				if p.lexer.Token != js_lexer.TColon {
+					p.skipTypeScriptType(js_ast.LPrefix)
+				}
 				break
 			}
 

internal/js_parser/ts_parser_test.go
@@ -117,6 +117,9 @@ func TestTSTypes(t *testing.T) {
 	expectPrintedTS(t, "let x: {['x']: number}", "let x;\n")
 	expectPrintedTS(t, "let x: {['x'](): void}", "let x;\n")
 	expectPrintedTS(t, "let x: {[key: string]: number}", "let x;\n")
+	expectPrintedTS(t, "let x: {[keyof: string]: number}", "let x;\n")
+	expectPrintedTS(t, "let x: {[readonly: string]: number}", "let x;\n")
+	expectPrintedTS(t, "let x: {[infer: string]: number}", "let x;\n")
 	expectPrintedTS(t, "let x: () => void = Foo", "let x = Foo;\n")
 	expectPrintedTS(t, "let x: new () => void = Foo", "let x = Foo;\n")
 	expectPrintedTS(t, "let x = 'x' as keyof T", "let x = \"x\";\n")

commit 46c9c18dd130c713597fef24654e5fb2e80ac223

fix bugs introduced by #1859

internal/js_parser/ts_parser.go
@@ -161,6 +161,7 @@ func (p *parser) skipTypeScriptType(level js_ast.L) {
 
 type skipTypeOpts struct {
 	isReturnType     bool
+	isIndexSignature bool
 	allowTupleLabels bool
 }
 
@@ -280,10 +281,15 @@ func (p *parser) skipTypeScriptTypeWithOpts(level js_ast.L, opts skipTypeOpts) {
 
 			if kind == tsTypeIdentifierPrefix {
 				p.lexer.Next()
-				// {[keyof: string]: number}
-				// {[readonly: string]: number}
-				// {[infer: string]: number}
-				if p.lexer.Token != js_lexer.TColon {
+
+				// Valid:
+				//   "[keyof: string]"
+				//   "{[keyof: string]: number}"
+				//
+				// Invalid:
+				//   "A extends B ? keyof : string"
+				//
+				if p.lexer.Token != js_lexer.TColon || (!opts.isIndexSignature && !opts.allowTupleLabels) {
 					p.skipTypeScriptType(js_ast.LPrefix)
 				}
 				break
@@ -507,7 +513,7 @@ func (p *parser) skipTypeScriptObjectType() {
 		if p.lexer.Token == js_lexer.TOpenBracket {
 			// Index signature or computed property
 			p.lexer.Next()
-			p.skipTypeScriptType(js_ast.LLowest)
+			p.skipTypeScriptTypeWithOpts(js_ast.LLowest, skipTypeOpts{isIndexSignature: true})
 
 			// "{ [key: string]: number }"
 			// "{ readonly [K in keyof T]: T[K] }"

internal/js_parser/ts_parser_test.go
@@ -120,6 +120,15 @@ func TestTSTypes(t *testing.T) {
 	expectPrintedTS(t, "let x: {[keyof: string]: number}", "let x;\n")
 	expectPrintedTS(t, "let x: {[readonly: string]: number}", "let x;\n")
 	expectPrintedTS(t, "let x: {[infer: string]: number}", "let x;\n")
+	expectPrintedTS(t, "let x: [keyof: string]", "let x;\n")
+	expectPrintedTS(t, "let x: [readonly: string]", "let x;\n")
+	expectPrintedTS(t, "let x: [infer: string]", "let x;\n")
+	expectParseErrorTS(t, "let x: A extends B ? keyof : string", "<stdin>: ERROR: Unexpected \":\"\n")
+	expectParseErrorTS(t, "let x: A extends B ? readonly : string", "<stdin>: ERROR: Unexpected \":\"\n")
+	expectParseErrorTS(t, "let x: A extends B ? infer : string", "<stdin>: ERROR: Unexpected \":\"\n")
+	expectParseErrorTS(t, "let x: {[new: string]: number}", "<stdin>: ERROR: Expected \"(\" but found \":\"\n")
+	expectParseErrorTS(t, "let x: {[import: string]: number}", "<stdin>: ERROR: Expected \"(\" but found \":\"\n")
+	expectParseErrorTS(t, "let x: {[typeof: string]: number}", "<stdin>: ERROR: Expected identifier but found \":\"\n")
 	expectPrintedTS(t, "let x: () => void = Foo", "let x = Foo;\n")
 	expectPrintedTS(t, "let x: new () => void = Foo", "let x = Foo;\n")
 	expectPrintedTS(t, "let x = 'x' as keyof T", "let x = \"x\";\n")